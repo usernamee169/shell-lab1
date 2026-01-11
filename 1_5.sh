75cf252cd4fe33f521fcd70e5a9c313387e232b5
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2026-01-11 14:53:04 +0300

    Update 1_5.sh

--- 1_5.sh
+++ 1_5.sh
@@ -1,3 +1,11 @@
+#5. На языке unix shell написать программу, которая заменяет имена файлов текущего каталога на их md5-сумму:
+#kartinko.jpg —> aab9fa58b9b58f00d1e6b037dc94c1c0.jpg
+#animashka.gif —> 4dbb610d0b0157c163a47d346d4c9e2c.gif
+#Cуффикс файла сохраняется
+
+
+
+
 #!/bin/bash
 
 for file in *; do
